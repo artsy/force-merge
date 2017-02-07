e2b46f9833ec59dad7d778541cbf8e91e598e561
Artsy <craigspaeth@gmail.com>
2017-02-07 16:00:17 -0500

    Config merged with script

---
+++ merge/config.js
@@ -0,0 +1,129 @@
+const { exec } = require('child_process')
+const { keys, omit, pick, map, extend, contains } = require('underscore')
+const fs = require('fs')
+const path = require('path')
+
+const newVars = {
+  NODE_ENV: 'development',
+  S3_BUCKET: 'force-merge',
+  APPLICATION_NAME: 'force-merge',
+  PORT: '5000',
+  APP_URL: 'http://localhost:5000'
+}
+
+const blacklistedVars = [
+  'NEW_RELIC_LOG',
+  'CDN_URL',
+  'ASSET_MANIFEST',
+  'NODE_MODULES_CACHE',
+  'PATH',
+  'OLD_S3_KEY',
+  'OLD_S3_SECRET',
+  'DEBUG',
+  'NEW_RELIC_APP_NAME',
+  'NEW_RELIC_BROWSER_MONITOR_ENABLE',
+  'NEW_RELIC_LOG',
+  'NEW_RELIC_LICENSE_KEY',
+  'COMMIT_HASH',
+  'OPENREDIS_URL',
+  'MARKETING_SIGNUP_MODAL_COPY',
+  'MARKETING_SIGNUP_MODAL_HEADER',
+  'MARKETING_SIGNUP_MODAL_IMG',
+  'MARKETING_SIGNUP_MODAL_SLUG',
+  'NPM_CONFIG_PRODUCTION',
+  'PORT'
+]
+
+const publicVars = [
+  'API_URL',
+  'APP_URL',
+  'METAPHYSICS_ENDPOINT',
+  'POSITRON_URL',
+  'PREDICTION_URL',
+  'ADMIN_URL',
+  'CMS_URL',
+  'GALAXY_URL',
+  'GEMINI_CLOUDFRONT_URL',
+  'MOBILE_URL',
+  'SECURE_IMAGES_URL',
+  'ARTSY_URL',
+  'FORCE_URL'
+]
+
+const dotEnvTemplate = `
+# OSS version of the .env file
+# Note: The ARTSY_ID & ARTSY_SECRET are known keys for Artsy OSS
+# projects, and are not a problem. OSS peeople: Please don't abuse the keys,
+# as then we'll have to change it, making it harder for others to learn from.
+# As such, these keys do not come under the Artsy security bounty either.
+
+# Shared
+__SHARED__
+
+# Migration
+OLD_S3_KEY=
+OLD_S3_SECRET=
+
+# Desktop
+__DESKTOP__
+
+# Mobile
+__MOBILE__
+`
+
+const write = (fname, str) =>
+  new Promise((resolve, reject) => {
+    const file = path.resolve(__dirname, '../') + '/' + fname
+    fs.writeFile(file, str, (err, res) => {
+      err ? reject(err) : resolve(res)
+    })
+  })
+
+const spawn = (cmd) =>
+  new Promise((resolve, reject) => exec(cmd, (err, stdout) => {
+    err ? reject(err) : resolve(stdout)
+  }))
+
+const config = async (app) => {
+  const str = await spawn(`heroku config --app=${app}`)
+  const hash = {}
+  str.split('\n').forEach((line) => {
+    const [key, ...rest] = line.split(':').map((str) => str.replace(/ /g, ''))
+    if (!(key.match('===') || key === '')) hash[key] = rest.join(':')
+  })
+  return hash
+}
+
+const toEnv = (hash, shared = false) => {
+  const vars = extend(omit(hash, blacklistedVars), shared ? newVars : {})
+  return map(vars, (v, k) => `${k}=${v}`).join('\n')
+}
+
+const toPublicEnv = (hash, shared = false) => {
+  const vars = extend(omit(hash, blacklistedVars), shared ? newVars : {})
+  return map(vars, (v, k) =>
+    `${k}=${contains(publicVars, k) ? v : ''}`
+  ).join('\n')
+}
+
+const main = async () => {
+  const desktop = await config('force-staging')
+  const mobile = await config('microgravity-staging')
+  const desktopOnly = omit(desktop, keys(mobile))
+  const mobileOnly = omit(mobile, keys(desktop))
+  const shared = pick(desktop, keys(mobile))
+  const dotEnv = dotEnvTemplate
+    .replace('__SHARED__', toEnv(shared, true))
+    .replace('__DESKTOP__', toEnv(desktopOnly))
+    .replace('__MOBILE__', toEnv(mobileOnly))
+  const dotEnvPublic = dotEnvTemplate
+    .replace('__SHARED__', toPublicEnv(shared, true))
+    .replace('__DESKTOP__', toPublicEnv(desktopOnly))
+    .replace('__MOBILE__', toPublicEnv(mobileOnly))
+  await Promise.all([
+    write('.env', dotEnv),
+    write('.env.oss', dotEnvPublic)
+  ])
+}
+
+main().catch(console.error.bind(console))
